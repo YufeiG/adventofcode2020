0af534b4c0417396f84a177b7608735d0a7ec8b7
Yufei Guo <yufeiguo@Yufeis-MacBook-Pro.local>
2020-12-01 23:03:14 -0800

    fix q1 to account for dup values in input

--- script1.py
+++ script1.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 def a():
 	# initial solution for part1
 	with open("input1.txt", "r") as f:
@@ -7,8 +9,7 @@ def a():
 		for i in range(t):
 			for j in range(i+1, t):
 				if int(numbs[i]) + int(numbs[j]) == 2020:
-					print("num: {}".format(int(numbs[i])*int(numbs[j])))
-					exit()
+					return int(numbs[i])*int(numbs[j])
 
 def b():
 	# initial solution for part2
@@ -20,8 +21,8 @@ def b():
 			for j in range(i+1, t):
 				for k in range(j+1, t):
 					if int(numbs[i]) + int(numbs[j]) + int(numbs[k]) == 2020:
-						print("num: {}".format(int(numbs[i])*int(numbs[j])*int(numbs[k])))
-						exit()
+						return int(numbs[i])*int(numbs[j])*int(numbs[k])
+
 def c():
 	# made it faster by casting to int up front
 	with open("input1.txt", "r") as f:
@@ -32,21 +33,36 @@ def c():
 			for j in range(i+1, t):
 				for k in range(j+1, t):
 					if numbs[i] + numbs[j] + numbs[k] == 2020:
-						print("num: {}".format(numbs[i]*numbs[j]*numbs[k]))
-						exit()
+						return numbs[i]*numbs[j]*numbs[k]
 
 def d():
 	# got rid of inner loop by using a set
 	with open("input1.txt", "r") as f:
 		string = f.read()
 		numbs = [int(x) for x in string.split()]
-		numSet = set(numbs)
+		numbDict = {}
 		t = len(numbs)
+
+		for i, v in enumerate(numbs):
+			x = numbDict.get(v, [])
+			x.append(i)
+			numbDict[v] = x
+
+		numSet = set(numbs)
 		for i in range(t):
 			for j in range(i+1, t):
-				s = 2020 - (numbs[i] + numbs[j])
-				if s in numSet:
-					print("num: {}".format(numbs[i]*numbs[j]*s))
-					exit()
+				a = numbs[i]
+				b = numbs[j]
+				s = 2020 - (a + b)
 
-d()
+				indices = numbDict.get(s)
+				if indices is not None:
+					assert len(indices) > 0
+					if i in indices:
+						indices.remove(i)
+					if j in indices:
+						indices.remove(j)
+					if len(indices) > 0:
+						return a*b*s
+
+print(d())

ee910e7af17740e2c4a7d04b9d90a4d038607308
Yufei <yufeiiguo@gmail.com>
2020-12-04 21:47:20 -0800

    refactor 5

--- script5.py
+++ script5.py
@@ -1,63 +1,29 @@
 #!/usr/bin/env python3
-
-def get_row(row_str):
-	upper = 128
+def calculate_num(id_str, char_for_upper, char_for_lower) -> int:
 	lower = 0
-	for r in row_str:
+	upper = 2 ** len(id_str)
+	for r in id_str:
 		h = (upper + lower)/2.0
-		if r == "F":
+		if r == char_for_lower:
 			upper = h
-		elif r == "B":
+		elif r == char_for_upper:
 			lower = h
 		else:
 			assert False
-	print("{} {}".format(upper, lower))
+	return int(lower)
 
-	return lower
-
-def get_col(col_str):
-	upper = 8
-	lower = 0
-	for r in col_str:
-		h = (upper + lower)/2.0
-		if r == "L":
-			upper = h
-		elif r == "R":
-			lower = h
-		else:
-			assert False
-	print("{} {}".format(upper, lower))
-
-	return lower
-
-def get_num(id_str):
+def get_seat_id(id_str) -> int:
 	assert len(id_str) == 10
-	first = id_str[:7]
-	second = id_str[7:]
-	print(first)
-	print(second)
-	row = get_row(first)
-	col = get_col(second)
+	row = calculate_num(id_str[:7], "B", "F")
+	col = calculate_num(id_str[7:], "R", "L")
 	return row*8 + col
 
 with open('input5.txt', 'r') as f:
-	max_numb = 0
-	all_ids = []
-	for r in f:
-		id_str = get_num(r.strip())
-		print(id_str)
-		all_ids.append(id_str)
-		if id_str > max_numb:
-			max_numb = id_str
-	print(max_numb)
+	all_ids = [get_seat_id(line.strip()) for line in f]
 	all_ids.sort()
+	print("Max: {}".format(all_ids[-1]))
 
-	for i, seat_id in enumerate(all_ids):
-		if i > 0 and i < len(all_ids) - 1:
-			before = all_ids[i-1]
-			after = all_ids[i+1]
-			if seat_id + 2 == after:
-				print(seat_id + 1)
-			if seat_id - 2 == before:
-				print(seat_id - 1)
-
+	for i, seat_id in enumerate(all_ids[:-1]):
+		after = all_ids[i+1]
+		if seat_id + 2 == after:
+			print("My seat: {}".format(seat_id + 1))

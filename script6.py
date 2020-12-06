259525e26164d19f6bfb785ecdd35a0e65c58da7
Yufei <yufeiiguo@gmail.com>
2020-12-05 21:21:42 -0800

    cleaned up q6

--- script6.py
+++ script6.py
@@ -1,30 +1,32 @@
 #!/usr/bin/env python3
 
+def intersection(set1, set2) -> set:
+	return set1.intersection(set2)
 
+def union(set1, set2) -> set:
+	return set1.union(set2)
+
+def process_answer_sets(answer_sets, set_lambda) -> int:
+	if len(answer_sets) == 0:
+		return 0
+	s = answer_sets[0]
+	for p in answer_sets[1:]:
+		s = set_lambda(s, p)
+	return len(s)
 
 with open('input6.txt', 'r') as f:
 	t = 0
-
 	people = []
+	action = intersection # or union for part 1
 	for line in f:
 		if line == '\n':
-			# process
-			if len(people) == 0:
-				continue
-			s = people[0]
-			for i, p in enumerate(people[1:]):
-				s = s.intersection(p)
-
-			t += len(s)
+			t += process_answer_sets(people, action)
 			people = []
 		else:
 			current = set()
 			for c in line.strip():
 				current.add(c)
 			people.append(current)
-	if len(people) > 0:
-		s = people[0]
-		for i, p in enumerate(people[1:]):
-			s = s.intersection(p)
-		t += len(s)
+
+	t += process_answer_sets(people, action)
 	print(t)

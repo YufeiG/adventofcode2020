8dfa16de4577e8d6f1384e0d63d5af44428cce63
Yufei <yufeiiguo@gmail.com>
2020-12-09 23:37:28 -0800

    more cleanup

--- script10.py
+++ script10.py
@@ -10,7 +10,7 @@ with open('input10.txt', 'r') as f:
 	previous_x = 0
 
 	consecutive_ones = 0
-	groups_of_ones = []
+	number_of_combinations = 1
 	for x in adapters + [adapters[-1] + 3 ]:
 		d = x - previous_x
 		if d == 1:
@@ -24,22 +24,16 @@ with open('input10.txt', 'r') as f:
 		if d == 1:
 			consecutive_ones += 1
 		elif consecutive_ones > 0:
-			groups_of_ones.append(consecutive_ones)
+			assert consecutive_ones < 5, consecutive_ones
+			# a difference of s means s + 1 adapters, but we can't
+			# remove the outer 2 adapters because they border a difference of 3,
+			# so we work with s - 1 adapters that we could potentially remove
+			# if there's only one 1, do nothing, one 1 between two 3s can never be removed
+			if consecutive_ones == 2 or consecutive_ones == 3:
+				number_of_combinations = number_of_combinations * (2**(consecutive_ones - 1))
+			elif consecutive_ones == 4:
+				number_of_combinations = number_of_combinations * ((2**(consecutive_ones - 1)) - 1) # minus the one case where all three 1s were removed
 			consecutive_ones = 0
 
 	print((one) * (three)) # 2048
-
-	product = 1
-	for s in groups_of_ones:
-		# a difference of s means s + 1 adapters, but we can't
-		# remove the outer 2 adapters because they border a difference of 3,
-		# so we work with s - 1 adapters that we could potentially remove
-		if s == 1:
-			continue # do nothing, one 1 between two 3s can never be removed
-		elif s == 2 or s == 3:
-			product = product * (2**(s - 1))
-		elif s == 4:
-			product = product * ((2**(s - 1)) - 1) # minus the one case where all three 1s were removed
-		else:
-			assert False, s
-	print(product) # 1322306994176
+	print(number_of_combinations) # 1322306994176

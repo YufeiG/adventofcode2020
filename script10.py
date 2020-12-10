d95c9af279a18e59ee52ccb076de57f5dc060132
Yufei <yufeiiguo@gmail.com>
2020-12-09 23:22:05 -0800

    clean up q10

--- script10.py
+++ script10.py
@@ -2,86 +2,45 @@
 import re
 
 with open('input10.txt', 'r') as f:
-	adapters = []
-	for x in f:
-		adapters.append(int(x.strip()))
+	adapters = [int(x.strip()) for x in f]
 	adapters.sort()
-	print(adapters)
 
 	one = 0
 	three = 0
 	previous_x = adapters[0]
-	assert previous_x == 1
-	differences = [1]
+	assert previous_x in [1, 2, 3]
+	differences = [previous_x]
 	for x in adapters[1:]:
-		if x - previous_x == 1:
+		d = x - previous_x
+		if d == 1:
 			one += 1
-		elif x - previous_x == 3:
+		elif d == 3:
 			three += 1
 		else:
-			assert False
-		differences.append(x - previous_x)
+			assert False, d
+		differences.append(d)
 		previous_x = x
 	print((1+one) * (1+three))
-	differences.append(3)
+	differences.append(3) # the device
 
-	# 
 	# differences contains 1 or 3
-	# never remove 3
-	# we can remove 1, 1,1, or 1,1,1, but never a 3
-	# count how many of the 1 patterns there are
-	print(differences)
 	consec_i = 0
-	ones = 0
-	b = 0
-	num_3s = 0
-	c = 0
 	summed_differences = []
 	for i in differences:
 		if i == 1:
 			consec_i += 1
-		else:
-			if consec_i == 0:
-				continue
+		elif consec_i > 0:
 			summed_differences.append(consec_i-1)
-			b += (consec_i - 1)
-			if consec_i == 1:
-				print("single")
-			elif consec_i <= 3:
-				ones += consec_i - 1
-				c += 2 ** (consec_i - 1)
-			elif consec_i == 4:
-				# calculate how many permutations of i works
-				ones += 3
-				num_3s += 1
-				c += 2 ** (consec_i - 1) - 1
-
-			else:
-				assert False, consec_i
-			print(consec_i)
 			consec_i = 0
-	m = b - (3*num_3s)
-	print((2**b) - (2**m))
-	# not 268435456
-	# not 18446744073709551616
-	# not 33554432
-	# not 268435591
-	# not 4294967296
-	# not 4398046511104
-	# not 4398046478336
-	running = 1
-	print(summed_differences)
+
+	product = 1
 	for s in summed_differences:
 		if s == 0:
-			print("zero")
+			continue # do nothing
 		elif s == 1 or s == 2:
-			running = running * (2**s)
+			product = product * (2**s)
 		elif s == 3:
-			p = (2 ** 3) - 1
-			running = running * p
+			product = product * ((2 ** s) - 1)
 		else:
 			assert False, s
-	print(running)
-
-
-
+	print(product) # 1322306994176

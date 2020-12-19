283d50955434e6bbbd0d2caf006b813826b47810
Yufei <yufeiiguo@gmail.com>
2020-12-19 00:17:55 -0800

    clean up q19

--- script19.py
+++ script19.py
@@ -10,49 +10,22 @@ base_rules = {}
 def convert_rule_to_regex_patterns(rule_n):
 	if rule_n == "8":
 		a = convert_rule_to_regex_patterns("42")
-		return "({})+".format(a)
-	if rule_n == "11":
-		a = convert_rule_to_regex_patterns("42")
-		b = convert_rule_to_regex_patterns("31")
-		r = "("
-		for i in range(1, 6):
-			r += f"({a}){{{i}}}({b}){{{i}}}|" 
-		r += f"({a}){{6}}({b}){{6}})" 
-		return r
+		return f"({a})+"
 
-	rule = rules[rule_n]
-	m1 = re.fullmatch("([0-9]+) ([0-9]+) \| ([0-9]+) ([0-9]+)", rule)
-	m2 = re.fullmatch("([0-9]+) ([0-9]+)", rule)
-	m3 = re.fullmatch("([0-9]+)", rule)
-	m4 = re.fullmatch("([0-9]+) \| ([0-9]+)", rule)
-	if m1 is not None or m2 is not None or m3 is not None or m4 is not None:
-		if m3 is not None:
-			return convert_rule_to_regex_patterns(m3.group(1))
-		if m2 is not None:
-			a = convert_rule_to_regex_patterns(m2.group(1))
-			b = convert_rule_to_regex_patterns(m2.group(2))
-			return a+b
-		if m1 is not None:
-			assert m1.group(1), rule
-			assert m1.group(2), rule
-			assert m1.group(3), m1.group(0)
-			assert m1.group(4), rule
-			a = convert_rule_to_regex_patterns(m1.group(1))
-			b = convert_rule_to_regex_patterns(m1.group(2))
-			c = convert_rule_to_regex_patterns(m1.group(3))
-			d = convert_rule_to_regex_patterns(m1.group(4))
-
-			return "({}{}|{}{})".format(a,b,c,d)
-		if m4 is not None:
-			a = convert_rule_to_regex_patterns(m4.group(1))
-			b = convert_rule_to_regex_patterns(m4.group(2))
-			return "({}|{})".format(a, b)
-		assert False
-	else:
-		# maybe a base rule?
-		assert rule_n in base_rules
+	if rule_n in base_rules:
 		return base_rules[rule_n]
+	rule = rules[rule_n]
 
+	if rule_n == "11":
+		rule = "42 31 | 42 42 31 31 | 42 42 42 31 31 31 | 42 42 42 42 31 31 31 31 | 42 42 42 42 42 31 31 31 31 31"
+
+	ors = rule.split("|")
+	values = []
+	for o in ors:
+		ns = o.strip().split(" ")
+		values.append("".join([convert_rule_to_regex_patterns(x) for x in ns]))
+	s = "|".join(values)
+	return f"({s})"
 
 def generate_base_rules():
 	for r, v in rules.items():
@@ -77,16 +50,19 @@ with open('input19.txt', 'r') as f:
 			rules[m.group(1)] = m.group(2)
 	rules["8"] = "42 | 42 8"
 	rules["11"] = "42 31 | 42 11 31"
+
 	generate_base_rules()
+
 	for r, v in rules.items():
-		if r not in base_rules and r not in regex_patterns:
+		if r not in base_rules:
 			reg = convert_rule_to_regex_patterns(r)
 			regex_patterns[r] = reg
+
 	rule_to_check = regex_patterns["0"]
 
 	n = 0
 	for x in inputs:
-		m = re.fullmatch("{}".format(rule_to_check), x)
+		m = re.fullmatch(rule_to_check, x)
 		if m is not None:
 			n += 1
 	print(n)
